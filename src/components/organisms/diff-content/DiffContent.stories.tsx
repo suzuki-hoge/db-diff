5d326f927e0826da068bd3607370d8d7cdd7b552
suzuki-hoge <user.ryo@gmail.com>
2023-08-04 10:24:50 +0900

    expand table cell.

--- src/components/organisms/diff-content/DiffContent.stories.tsx
+++ src/components/organisms/diff-content/DiffContent.stories.tsx
@@ -390,3 +390,64 @@ export const RowModifiedOnlyUniqueColumn: Story = {
     },
   },
 }
+
+export const LargeDiff: Story = {
+  args: {
+    tableDiff: {
+      tableName: 'users',
+      primaryValues: ['1', '23456'],
+      primaryColName: 'id',
+      colNames: ['name-value', 'age', 'session', 'note', 'created', 'updated'],
+      rowDiffs1: {
+        '1': {
+          'name-value': { status: 'stay', value: '"John"' },
+          age: { status: 'stay', value: '29' },
+          session: {
+            status: 'deleted',
+            value:
+              '"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ"',
+          },
+          note: {
+            status: 'deleted',
+            value:
+              '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."',
+          },
+          created: { status: 'stay', value: '"2023-06-12 11:30:56"' },
+          updated: { status: 'deleted', value: '"2023-06-15 08:42:05"' },
+        },
+      },
+      rowDiffs2: {
+        '1': {
+          'name-value': { status: 'stay', value: '"Alexander"' },
+          age: { status: 'stay', value: '29' },
+          session: {
+            status: 'added',
+            value:
+              '"eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJhh994RAPzCG0hmQ"',
+          },
+          note: {
+            status: 'added',
+            value:
+              '"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n"',
+          },
+          created: { status: 'stay', value: '"2023-06-12 11:30:56"' },
+          updated: { status: 'added', value: '"2023-06-17 23:00:17"' },
+        },
+        '23456': {
+          'name-value': { status: 'added', value: '"Smith"' },
+          age: { status: 'added', value: '42' },
+          session: {
+            status: 'added',
+            value: '<null>',
+          },
+          note: {
+            status: 'added',
+            value: '',
+          },
+          created: { status: 'added', value: '"2023-06-12 11:30:56"' },
+          updated: { status: 'added', value: '"2023-06-17 23:00:17"' },
+        },
+      },
+    },
+  },
+}
--- src/components/organisms/diff-content/DiffContent.tsx
+++ src/components/organisms/diff-content/DiffContent.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, Fragment } from 'react'
+import React, { type FC, Fragment, type MouseEventHandler, useEffect, useRef, useState } from 'react'
 import { type TableDiff } from '../../../types'
 import styles from './DiffContent.module.scss'
 
@@ -23,18 +23,22 @@ interface ColsRowProps {
 const ColsRow: FC<ColsRowProps> = (props) => {
   return (
     <tr>
-      {props.n === 1 && <td rowSpan={2}>{props.primaryValue}</td>}
+      {props.n === 1 && (
+        <td rowSpan={2} align={'left'}>
+          {props.primaryValue}
+        </td>
+      )}
       {props.colNames.map((colName, i) =>
         props.rowDiff !== undefined ? (
           colName in props.rowDiff ? (
-            <td key={i} className={colors[props.rowDiff[colName].status]}>
+            <td key={i} className={colors[props.rowDiff[colName].status]} align={'left'}>
               {props.rowDiff[colName].value}
             </td>
           ) : (
-            <td key={i} className={colors.none}></td>
+            <td key={i} className={colors.none} align={'left'}></td>
           )
         ) : (
-          <td key={i} className={colors.none}></td>
+          <td key={i} className={colors.none} align={'left'}></td>
         )
       )}
     </tr>
@@ -52,29 +56,123 @@ const PrimaryOnlyRow: FC<PrimaryOnlyRowProps> = (props) => {
     <>
       {props.rowDiff1 && (
         <tr>
-          <td className={[styles.primaryOnly, colors.deleted].join(' ')}>{props.primaryValue}</td>
+          <td className={[styles.primaryOnly, colors.deleted].join(' ')} align={'left'}>
+            {props.primaryValue}
+          </td>
         </tr>
       )}
       {props.rowDiff2 && (
         <tr>
-          <td className={[styles.primaryOnly, colors.added].join(' ')}>{props.primaryValue}</td>
+          <td className={[styles.primaryOnly, colors.added].join(' ')} align={'left'}>
+            {props.primaryValue}
+          </td>
         </tr>
       )}
     </>
   )
 }
 
+const Resizer: FC<{ cellId: string; resizerId: string; height: number }> = (props) => {
+  let x = 0
+  let w = 0
+
+  const downHandler: MouseEventHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    x = e.clientX
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    w = parseInt(window.getComputedStyle(document.getElementById(props.cellId)!).width, 10)
+
+    document.addEventListener('mousemove', moveHandler)
+    document.addEventListener('mouseup', upHandler)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.getElementById(props.resizerId)!.classList.add('resizing')
+  }
+
+  const moveHandler: (event: MouseEvent) => void = (e: MouseEvent) => {
+    const dx = e.clientX - x
+    console.log(x, w, dx, w + dx)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.getElementById(props.cellId)!.style.width = `${w + dx}px`
+  }
+
+  const upHandler: (event: MouseEvent) => void = () => {
+    document.removeEventListener('mousemove', moveHandler)
+    document.removeEventListener('mouseup', upHandler)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    document.getElementById(props.resizerId)!.classList.remove('resizing')
+  }
+
+  return <div id={props.resizerId} className={styles.resizer} style={{ height: `${props.height}px` }} onMouseDown={downHandler} />
+}
+
 export const DiffContent: FC<Props> = (props) => {
+  const table = useRef<HTMLTableElement>(null)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    if (table.current !== null) {
+      setHeight(table.current.offsetHeight - 2) // table border
+    } else {
+      setHeight(0)
+    }
+  }, [table])
+
+  const [expand, setExpand] = useState(false)
+
+  const rows = [...Object.values(props.tableDiff.rowDiffs1), ...Object.values(props.tableDiff.rowDiffs2)]
+  const expandLens = props.tableDiff.colNames.map((colName) => {
+    return [colName.length, ...rows.map((row) => row[colName].value.length)].reduce((a, b) => (a > b ? a : b))
+  })
+  const ellipsisLens = props.tableDiff.colNames.map((colName) => {
+    const fullLen = rows.map((row) => row[colName].value.length).reduce((a, b) => (a > b ? a : b))
+    return fullLen < colName.length ? colName.length : Math.min(22, fullLen)
+  })
+  const primaryLen = [...props.tableDiff.primaryValues.map((v) => v.length), props.tableDiff.primaryColName.length].reduce((a, b) => (a > b ? a : b))
+  const cols = [primaryLen, ...ellipsisLens].length
+  const sumPx =
+    primaryLen * 10 + // primary
+    expandLens.reduce((a, b) => a + b, 0) * 10 + // cols
+    cols * 16 * 2 + // paddings
+    (cols - 1) + // cell borders
+    2 // table border
+
   return (
-    <div id={props.tableDiff.tableName} className={styles.component}>
+    <div id={props.tableDiff.tableName} className={styles.component} style={{ width: `${sumPx}px` }}>
       <span className={styles.label}>{props.tableDiff.tableName}</span>
-      <table>
+      <button
+        onClick={() => {
+          setExpand(!expand)
+        }}
+      >
+        f
+      </button>
+      <table ref={table}>
         <thead>
           <tr>
-            <th>{props.tableDiff.primaryColName}</th>
-            {props.tableDiff.colNames.map((colName, i) => (
-              <th key={i}>{colName}</th>
-            ))}
+            <th align={'left'} style={{ width: `${primaryLen * 10}px` }}>
+              {props.tableDiff.primaryColName}
+            </th>
+            {props.tableDiff.colNames.map((colName, i) => {
+              const cellId = `${props.tableDiff.tableName}-${i}`
+              return (
+                <th
+                  key={i}
+                  id={cellId}
+                  align={'left'}
+                  style={
+                    expand
+                      ? { width: `${expandLens[i] * 10}px`, maxWidth: `${expandLens[i] * 10}px` }
+                      : { width: `${ellipsisLens[i] * 10}px`, maxWidth: `${expandLens[i] * 10}px` }
+                  }
+                >
+                  {colName}
+                  <Resizer key={i} cellId={cellId} resizerId={`resizer-${i}`} height={height} />
+                </th>
+              )
+            })}
           </tr>
         </thead>
         <tbody>
